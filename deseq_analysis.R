4e6e7a6993040723d4376e71f3c2ac20adf4dfe6
Anokhi Haria <113692983+aharia22@users.noreply.github.com>
2023-11-23 14:18:04 -0500

    Updated deseq_analysis.R
    with these changes the gene names are visible in the DE_Seq csv file

--- deseq_analysis.R
+++ deseq_analysis.R
@@ -10,18 +10,36 @@ counts <- read.delim("GSE103584_R01_NSCLC_RNAseq.txt", header=TRUE, row.names=1)
 
 #replace NAs with zeros
 counts[is.na(counts)] <- 0
-counts <- as.data.frame(lapply(counts, as.integer))
+#apply the integer conversion to all columns except the first
+counts[, -1] <- lapply(counts[, -1], as.integer)
+counts$`R01.023` <- as.integer(counts$`R01.023`)
+counts$`R01.024` <- as.integer(counts$`R01.024`)
 
 
 #read in metadata (sample information)
 colData <- read.delim("sample_metadata.txt", header=TRUE, row.names=1)
+rownames(colData) <- gsub("-", ".", rownames(colData))
 
-#making sure that the column names of count data match the row names of colData
-colnames(counts) <- colData$sample
+#then we use these row names as the column names for counts
+colnames(counts) <- rownames(colData)
+#Convert the treatment column in colData to a factor
 colData$treatment <- as.factor(colData$treatment)
 
+#check for NA values in the counts matrix
+if (any(is.na(counts))) {
+  cat("NA values found in the counts matrix.\n")
+  # Replace NA values with zeros
+  counts[is.na(counts)] <- 0
+}
 
-#create a DESeqDataSet object
+#confirm that there are no more NA values
+if (any(is.na(counts))) {
+  cat("NA values are still present after replacement.\n")
+} else {
+  cat("No NA values are present. Proceeding with DESeq2.\n")
+}
+
+#proceed with DESeq2 if no NAs are present
 dds <- DESeqDataSetFromMatrix(countData = counts, colData = colData, design = ~ treatment)
 
 #run the DESeq pipeline
@@ -34,4 +52,4 @@ res <- results(dds, contrast=c("treatment", "male", "female"))
 resOrdered <- res[order(res$pvalue),]
 
 #exporting results to csv
-write.csv(as.data.frame(resOrdered), file="DESeq2_results.csv")
+write.csv(as.data.frame(resOrdered), file="DESeq2_results.csv", row.names=TRUE)
